fbd8afa5ba51ce74fb51642f845a66121bd191bb
penelope-in-piedmont <penelope.b.welch@gmail.com>
2023-09-24 18:50:29 -0700

    updating code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/Auto.java
@@ -21,9 +21,7 @@ public abstract class Auto extends LinearOpMode {
         leftBackDrive = hardwareMap.get(DcMotor.class, Config.LEFT_BACK_DRIVE);
         rightFrontDrive = hardwareMap.get(DcMotor.class, Config.RIGHT_FRONT_DRIVE);
         rightBackDrive = hardwareMap.get(DcMotor.class, Config.RIGHT_BACK_DRIVE);
-        triggerMotor = hardwareMap.get(DcMotor.class, Config.TRIGGER_MOTOR);
-        flywheelMotor = hardwareMap.get(DcMotor.class, Config.FLYWHEEL_MOTOR);
-        turnTable = hardwareMap.get(DcMotor.class, Config. TURN_TABLE);
+
     }
         public void moveBackward(long milliseconds) {
         leftFrontDrive.setPower(-1);
