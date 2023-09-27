dfd7f2f2837aab4752b45e0c006780d7841e37c0
millu <Lukey1000two@gmail.com>
2023-09-27 16:15:27 -0700

    changed the variables existence

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/SpikeparkBlueside.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/SpikeparkBlueside.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 @Autonomous(name="SpikeParkBlue")
 public class SpikeparkBlueside extends Auto{
     public int forward = 1500;
-    public int sRightLeft = 450;
+    public int sRightLeft = 200;
     public int sLeftright2 = 2000;
     public int side = 200;
     @Override
