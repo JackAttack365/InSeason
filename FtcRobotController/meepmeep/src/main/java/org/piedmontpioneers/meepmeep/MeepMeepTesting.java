15092385e5e6c937229448d58d68306340514bcb
SCool62 <79726681+SCool62@users.noreply.github.com>
2023-10-11 16:46:39 -0700

    Update MeepMeep

--- FtcRobotController/meepmeep/src/main/java/org/piedmontpioneers/meepmeep/MeepMeepTesting.java
+++ FtcRobotController/meepmeep/src/main/java/org/piedmontpioneers/meepmeep/MeepMeepTesting.java
@@ -1,6 +1,6 @@
 package org.piedmontpioneers.meepmeep;
 
-import com.acmerobotics.roadrunner.geometry.Pose2d;
+
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -11,21 +11,14 @@ public class MeepMeepTesting {
         MeepMeep meepMeep = new MeepMeep(800);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
-                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive ->          // You can change the start and heading
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                // Tragectory Sequence goes here!
-                                .build()
-                );
+                // Sequence goes here
+                .build();
 
-        // TODO: Update MeepMeep when the new field comes out.
-        meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
+        meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
                 .start();
     }
-
 
 }
