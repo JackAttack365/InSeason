37da1ea6f17216650610871ba7e870de6610e8c4
SCool62 <79726681+SCool62@users.noreply.github.com>
2023-10-11 16:53:12 -0700

    Fix meepmeep

--- FtcRobotController/meepmeep/src/main/java/org/piedmontpioneers/meepmeep/MeepMeepTesting.java
+++ FtcRobotController/meepmeep/src/main/java/org/piedmontpioneers/meepmeep/MeepMeepTesting.java
@@ -1,7 +1,9 @@
 package org.piedmontpioneers.meepmeep;
 
 
+import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
@@ -12,7 +14,13 @@ public class MeepMeepTesting {
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Sequence goes here
-                .build();
+                .setColorScheme(new ColorSchemeBlueDark())
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
+                                 // Sequence Here!
+                                .build()
+                );
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
                 .setDarkMode(true)
