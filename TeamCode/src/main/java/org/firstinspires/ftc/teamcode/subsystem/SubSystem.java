36c0bc014f3dc147a9c5bf0e3958b717d0c353b1
SCool62 <79726681+SCool62@users.noreply.github.com>
2023-10-25 16:22:07 -0700

    Set up one controller mode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/SubSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/SubSystem.java
@@ -4,9 +4,16 @@ import org.firstinspires.ftc.teamcode.Config;
 
 public abstract class SubSystem {
     protected final Config config;
+    protected final boolean isOneController;
 
     public SubSystem(Config config) {
         this.config = config;
+        this.isOneController = false;
+    }
+
+    public SubSystem(Config config, boolean isOneController) {
+        this.config = config;
+        this.isOneController = isOneController;
     }
     public abstract void init();
     public abstract void update();
