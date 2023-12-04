0fbf56e8b059d95d986dfc0d7f98b192e19d530c
SCool62 <79726681+SCool62@users.noreply.github.com>
2023-12-03 18:36:01 -0800

    Add comment

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/AirplaneLauncher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/AirplaneLauncher.java
@@ -5,10 +5,10 @@ import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.teamcode.Config;
 
 public class AirplaneLauncher extends SubSystem{
+
     public AirplaneLauncher(Config config) {
         super(config);
     }
-
     private Servo airplaneServo;
 
     @Override
@@ -19,6 +19,7 @@ public class AirplaneLauncher extends SubSystem{
 
     @Override
     public void update() {
+        // Releases the launcher when Start-B controller's B button is pressed
         // todo: tune position values
         if (config.gamePad2.b) {
             airplaneServo.setPosition(0.13);
