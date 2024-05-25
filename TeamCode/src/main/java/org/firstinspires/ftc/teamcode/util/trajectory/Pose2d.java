b271a8c62285c23dca6f7cbfaf9b7570a2114279
JackAttack-0 <142643773+JackAttack365@users.noreply.github.com>
2024-05-25 11:41:40 -0700

    Worky work work

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/trajectory/Pose2d.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.util.trajectory;
+
+public class Pose2d {
+    public double x;
+    public double y;
+    public double heading;
+
+    public Loc2d(double x, double y, double heading) {
+        this.x = x;
+        this.y = y;
+        this.heading = heading;
+    }
+}
